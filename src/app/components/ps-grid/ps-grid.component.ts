9cadfb14db6cd690fe0f8972b43607c299d2ee0f
ihago <ihago.g3.system@gmail.com>
2023-09-23 18:30:51 -0300

    ps-grid created

---
+++ src/app/components/ps-grid/ps-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ps-grid',
+  templateUrl: './ps-grid.component.html',
+  styleUrls: ['./ps-grid.component.css']
+})
+export class PsGridComponent {
+
+}
